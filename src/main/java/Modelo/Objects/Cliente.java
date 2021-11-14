01382bd9913578d397249896ff8cd9287ba813e7
Camilo Ternera <camiloterneraduque@gmail.com>
2021-11-13 23:17:31 -0500

    feat:  select cliente

--- src/main/java/Modelo/Objects/Cliente.java
+++ src/main/java/Modelo/Objects/Cliente.java
@@ -5,15 +5,19 @@ public class Cliente {
     private int id_cliente; 
     private String nombre;
     private String apellidos;
+    private String segundoApellidos;
+    private String nombreCompleto;
     private int documento;
     private String direccion;
     private String telefono;
     private String celular;
 
-    public Cliente(int id_cliente, String nombre, String apellidos, int documento, String direccion, String telefono, String celular) {
+    public Cliente(int id_cliente, String nombre, String apellidos, String segundoApellidos, String nombreCompleto, int documento, String direccion, String telefono, String celular) {
         this.id_cliente = id_cliente;
         this.nombre = nombre;
         this.apellidos = apellidos;
+        this.segundoApellidos = segundoApellidos;
+        this.nombreCompleto = nombreCompleto;
         this.documento = documento;
         this.direccion = direccion;
         this.telefono = telefono;
@@ -46,6 +50,22 @@ public class Cliente {
 
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellidos;
+    }
+
+    public void setSegundoApellido(String segundoApellidos) {
+        this.segundoApellidos = segundoApellidos;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
     }
 
     public int getDocumento() {
